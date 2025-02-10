118ce73702012194599b793222940ede724355e6
Berllock <brenosouzaemp@gmail.com>
2025-02-10 17:12:48 -0300

    Remove HardCode

--- gerar_qrcode.py
+++ gerar_qrcode.py
@@ -9,11 +9,16 @@ qr = qrcode.QRCode(
     border=3  # Define a largura da borda
 )
 
+# Requere a entrada de dados do QR Code
+qr_Data = input("Insira o dado para o QRCode: ")
+# Requere o nome e formato do arquivo que será gerado
+qr_Name = input("Insira o nome do arquivo e formato: ")
+
 # Adiciona os dados ao QR Code
-qr.add_data("https://senhoralgodao.com.br/")
+qr.add_data(qr_Data)
 qr.make(fit=True)  # Ajusta o QR Code para se ajustar aos dados
 
 # Gera a imagem do QR Code com as cores especificadas
-imagem = qr.make_image(fill_color="Purple", back_color="white")
+imagem = qr.make_image()
 # Salva a imagem gerada em um arquivo
-imagem.save("QRSrAlgodão.jpg")
+imagem.save(qr_Name)
